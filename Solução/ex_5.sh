4ad7b33fabfe24687e2821737e55631dbbe64774
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-03-31 11:35:35 -0300

    correção do exercicio 3

--- Solução/ex_5.sh
+++ Solução/ex_5.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-cat ../Dados/machado/*/* | sed 's/ /\n/g' | sed '/^$/d' | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | iconv -f ISO-8859-1 -t UTF-8 | sort -b | uniq -c | sort -rn > resposta_ex_5.txt
+cat ../Dados/machado/*/* | sed 's/ /\n/g' | sed '/^$/d' | tr '[:upper:]' '[:lower:]' | tr -d '[:punct:]' | iconv -f ISO-8859-1 -t UTF-8 | sort -b | uniq -c | sort | sed 's/^[ \t]*//' > resposta_ex_5.txt
