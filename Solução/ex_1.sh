725865ba571c389968b5178ab65826e8a27a868f
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-03-31 14:57:43 -0300

    exercício 1 completo

--- Solução/ex_1.sh
+++ Solução/ex_1.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 
-echo "
-cd ..
+wc -w ../Dados/machado/*/*.txt | tail -n 1 | awk '{print $1}' > resposta_ex_1.txt
+echo "palavras" >> resposta_ex_1.txt
 
-wc -w Dados/machado/*/*.txt
-"
-minha-resposta="2482716 palavras"
-echo $minha-resposta > resposta_ex_1.txt
+
+# echo "
+# cd ..
+
+# wc -w Dados/machado/*/*.txt
+# "
+# minha-resposta="2482716 palavras"
+# echo $minha-resposta > resposta_ex_1.txt
