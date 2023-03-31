ef4609a0e0b1c35a45f4af57774b52b7f8055fb9
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-03-31 08:52:49 -0300

    correcao exercicio 3

--- Solução/ex_3.sh
+++ Solução/ex_3.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-awk 'FNR==1 {print $0}' Dados/machado/*/*.txt > pLinha.txt | sed 's/, */, /g'
+awk 'FNR==1 {print $0}' ../Dados/machado/*/*.txt > pLinha.txt | sed 's/, */, /g'
 iconv -f ISO-8859-1 -t UTF-8 pLinha.txt | awk -F', ' '{ $0= $NF " " $0;print $0 |"sort -k1"}' > resposta_ex_3.txt
 sed -i 's/^[^.,]*[.,]//' resposta_ex_3.txt
 sed -i 's/^[ \t]*//' resposta_ex_3.txt
